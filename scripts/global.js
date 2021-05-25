c9760f5e6bba81df6dcf1105ffb6a1d008cfec46
Renmauzuo <renmauzuo@yahoo.com>
2021-05-25 00:43:21 -0500

    WIP commit of ability scores.
    Added calculations for ability scores, and averages for strength.
    
    Refactored some of the logic used to calculate size to be in reusable functions.

--- scripts/global.js
+++ scripts/global.js
@@ -12,6 +12,9 @@ $(function () {
 
 });
 
+/**
+ * Scales mosnter stats based on the selected template and challenge rating.
+ */
 function calculateSelectedMonster() {
     let selectedMonster = monsterList[$('#monster-select').val()];
     let targetCR = $('#cr-select').val();
@@ -24,66 +27,43 @@ function calculateSelectedMonster() {
     //For this reason we do the interpolation for EACH stat individually, rather than finding the closest statblock to draw from
 
     if(!derivedStats.size) {
-        let upperCR, upperSize, lowerCR, lowerSize;
-        for (let cr in selectedMonster.stats) {
-            let statBlock = selectedMonster.stats[cr];
-            if (statBlock.size) {
-                if (cr > targetCR) {
-                    if (!upperCR || upperCR > cr) {
-                        upperCR = cr;
-                        upperSize = statBlock.size;
-                    }
-                } else {
-                    if (!lowerCR || lowerCR < cr) {
-                        lowerCR = cr;
-                        lowerSize = statBlock.size;
-                    }
-                }
-            }
-        }
-
-        //If a benchmark was only found in one direction we simply use that benchmark to extrapolate a state for the target CR
-        //If benchmarks were found above and below, we calculate the target result for BOTH benchmarks, then take a weighted average based on which is closer
-        //So if the upper benchmark is 1 step away, and the lower benchmark is 4 steps away, then the upper will count for 80% of the average
-        let upperValue, lowerValue;
-        if (upperCR) {
-            let offset = upperSize - averageStats[upperCR].size;
-            upperValue = offset + averageStats[targetCR].size;
-        }
-        if (lowerCR) {
-            let offset = lowerSize - averageStats[lowerCR].size;
-            lowerValue = offset + averageStats[targetCR].size;
-        }
-
-        if (lowerValue) {
-            if (upperValue) {
-                //If upper and lower take a weighted average
-                let upperStep = stepForCR(upperCR);
-                let lowerStep = stepForCR(lowerCR);
-                let stepRange = upperStep - lowerStep;
-                let targetStep = stepForCR(targetCR);
-                let upperWeight = (upperStep - targetStep) / stepRange;
-                let lowerWeight = (targetStep - lowerStep) / stepRange;
-                derivedStats.size = upperWeight * upperValue + lowerWeight * lowerValue;
-            } else {
-                derivedStats.size = lowerValue;
-            }
-        } else {
-            derivedStats.size = upperValue;
-        }
-
-        console.log("Closest above: " + upperValue + ' Closest Below: ' + lowerValue);
+        let sizeBenchmarks = findBenchmarksForStat("size", targetCR, selectedMonster);
+        derivedStats.size = extrapolateFromBenchmark("size", targetCR, sizeBenchmarks, true);
+        derivedStats.size = Math.min(6, Math.round(derivedStats.size));
     }
+
+    let abilityScores = ["str", "con", "dex", "int", "wis", "cha"];
+    derivedStats.abilityModifiers = {};
+    for (let i = 0; i < abilityScores.length; i++) {
+        if (!derivedStats[abilityScores[i]]) {
+            let abilityBenchmarks = findBenchmarksForStat(abilityScores[i], targetCR, selectedMonster);
+            derivedStats[abilityScores[i]] = Math.round(extrapolateFromBenchmark(abilityScores[i], targetCR, abilityBenchmarks, false));
+        }
+        derivedStats.abilityModifiers[abilityScores[i]] = abilityScoreModifier(derivedStats[abilityScores[i]]);
+    }
+
+
     console.log(JSON.stringify(derivedStats));
 
     //Once we have all the stats populate the statblock:
     $('#monster-name').html(selectedMonster.slug);
-    $('#monster-subtitle').html(sizes[Math.round(derivedStats.size)].name + ' ' + selectedMonster.type + ', ' + selectedMonster.alignment);
+    $('#monster-subtitle').html(sizes[derivedStats.size].name + ' ' + selectedMonster.type + ', ' + selectedMonster.alignment);
+    for (let i = 0; i < abilityScores.length; i++) {
+        let abilityScore = abilityScores[i];
+        let modifier = abilityScoreModifier(derivedStats[abilityScore]);
+        let modifierString = "(" + (modifier >= 0 ? '+' : '') + modifier + ")";
+       $('#monster-'+abilityScore).html(derivedStats[abilityScore] + " " + modifierString);
+    }
 }
 
-//Fractional CRs are counted as a full step in calculations, ie going from CR 1/8 to 1/4 carries as much weight as going from CR 1 to 2.
-//This function converts CR into a "step" to ease calculations elsewhere
+/**
+ * Converts challenge rating to a "step" so that fractional CRs carry the same weight in scaling as full number CRs.
+ *
+ * @param {string} cr The challenge rating to convert to a step.
+ * @return {number} The relative step for the challenge rating.
+ */
 function stepForCR(cr) {
+    //Fractional CRs are counted as a full step in calculations, ie going from CR 1/8 to 1/4 carries as much weight as going from CR 1 to 2.
     let safeCR = parseFloat(cr);
     switch(safeCR) {
         case 0:
@@ -97,5 +77,107 @@ function stepForCR(cr) {
         default:
             return safeCR+3;
     }
+}
 
+/**
+ * Finds the closest statblocks above and below the target CR that have the target stat
+ *
+ * @param {Array} stats The stats to search for
+ * @param {string} targetCR The challenge rating to find benchmarks for
+ * @param {Object} selectedMonster The monster template for which to find stat benchmarks
+ * @return {Object} Benchmarks for the selected stat at the nearest CRs above and below it that had values for that stat.
+ */
+function findBenchmarksForStat(stats, targetCR, selectedMonster) {
+    let statList = Array.isArray(stats) ? stats : [stats];
+    let benchmarks = {}
+    for (let cr in selectedMonster.stats) {
+        let statBlock = selectedMonster.stats[cr];
+        let allStatsFound = true;
+        for (let i = 0; i < statList.length; i++) {
+            allStatsFound = allStatsFound && statBlock[statList[i]];
+        }
+        if (allStatsFound) {
+            if (cr > targetCR) {
+                if (!benchmarks.upper || benchmarks.upper.cr > cr) {
+                    benchmarks.upper = {
+                        cr: cr,
+                    }
+                    for (let i = 0; i < statList.length; i++) {
+                        benchmarks.upper[statList[i]] = statBlock[statList[i]];
+                    }
+                }
+            } else {
+                if (!benchmarks.lower || benchmarks.lower.cr < cr) {
+                    benchmarks.lower = {
+                        cr: cr,
+                    }
+                    for (let i = 0; i < statList.length; i++) {
+                        benchmarks.lower[statList[i]] = statBlock[statList[i]];
+                    }
+                }
+            }
+        }
+    }
+    return benchmarks;
+}
+
+//Extrapolates the value for a stat at a target CR 
+/**
+ * Finds the closest statblocks above and below the target CR that have the target stat
+ *
+ * @param {string} stat The stat to extrapolate
+ * @param {string} targetCR The challenge rating to find benchmarks for
+ * @param {Object} benchmarks The upper and/or lower benchmarks to extrapolate from
+ * @param {boolean} linearExtrapolation If true the extrapolation will be an offset instead of a ratio.
+ *  For example, a template with a value of 5 when the average stat is 4 would result in an offset of +1 instead of a multiplier of *1.2.
+ * @return {Number} The extrapolated value
+ */
+function extrapolateFromBenchmark(stat, targetCR, benchmarks, linearExtrapolation) {
+    //If a benchmark was only found in one direction we simply use that benchmark to extrapolate a state for the target CR
+    //If benchmarks were found above and below, we calculate the target result for BOTH benchmarks, then take a weighted average based on which is closer
+    //So if the upper benchmark is 1 step away, and the lower benchmark is 4 steps away, then the upper will count for 80% of the average
+    let upperValue, lowerValue;
+    if (benchmarks.upper) {
+        if (linearExtrapolation) {
+            let offset = benchmarks.upper[stat] - averageStats[benchmarks.upper.cr][stat];
+            upperValue = offset + averageStats[targetCR][stat];
+        } else {
+            let ratio = benchmarks.upper[stat] / averageStats[benchmarks.upper.cr][stat];
+            upperValue = ratio * averageStats[targetCR][stat];
+        }
+    }
+    if (benchmarks.lower) {
+        if (linearExtrapolation) {
+            let offset = benchmarks.lower[stat] - averageStats[benchmarks.lower.cr][stat];
+            lowerValue = offset + averageStats[targetCR][stat];
+        } else {
+            let ratio = benchmarks.lower[stat] / averageStats[benchmarks.lower.cr][stat];
+            lowerValue = ratio * averageStats[targetCR][stat];
+        }
+    }
+
+    if (lowerValue) {
+        if (upperValue) {
+            //If upper and lower take a weighted average
+            let upperStep = stepForCR(benchmarks.upper.cr);
+            let lowerStep = stepForCR(benchmarks.lower.cr);
+            let stepRange = upperStep - lowerStep;
+            let targetStep = stepForCR(targetCR);
+            let upperWeight = (upperStep - targetStep) / stepRange;
+            let lowerWeight = (targetStep - lowerStep) / stepRange;
+            return upperWeight * upperValue + lowerWeight * lowerValue;
+        }
+        return lowerValue;
+    }
+    return upperValue;
+}
+
+/**
+ * Calcualtes the modifier for an ability score
+ *
+ * @param {string} ability The ability score
+ * @return {number} The ability score modifier
+ */
+ function abilityScoreModifier(ability) {
+    return Math.floor((ability - 10) / 2);
 }
--- scripts/monsters.js
+++ scripts/monsters.js
@@ -23,32 +23,26 @@ const monsterList = {
         type: "beast",
         alignment: "unaligned",
         lockedStats: {
-            abilityScores: {
-                dex: 15,
-                int: 3,
-                wis: 12,
-            }
+            dex: 15,
+            int: 3,
+            wis: 12,
         },
         stats: {
             .25: {
                 naturalArmor: 1,
                 speed: 40,
                 size: sizeMedium,
-                abilityScores: {
-                    str: 12,
-                    con: 12,
-                    cha: 6
-                }
+                str: 12,
+                con: 12,
+                cha: 6
             },
             1 : {
                 naturalArmor: 2,
                 speed: 50,
                 size: sizeLarge,
-                abilityScores: {
-                    str: 17,
-                    con: 15,
-                    cha: 7
-                }
+                str: 17,
+                con: 15,
+                cha: 7
             }
         }
     }
@@ -59,110 +53,178 @@ const monsterList = {
 //For example, a creature with above average strength for its CR will continue to have above average strength at other CRs
 
 //TODO: Consider adding different averages by type.
-//Note on sizes: Average sizes were determined by average size of monsters in various rulebooks by CR. However, outliers sometimes skewed results for certain CRs, especially at high levels when there are very few monsters at each CR, so many of these vlaues have been "fudged" to create a smoother upward transition.
-
+//Most of these stats were determined by average size of monsters in various rulebooks by CR. However, outliers sometimes skewed results for certain CRs, especially at high levels when there are very few monsters at each CR, so many of these vlaues have been "fudged" to create a smoother upward transition.
+//Decimal values are to ease the curve a bit, even though all final values will be rounded.
 
 const averageStats = {
     0 : {
+        ac: 11, 
+        str: 5,
         size: sizeSmall - .5 //Low end of small
     },
     .125 : {
+        ac: 12, 
+        str: 9,
         size: sizeSmall
     },
     .25 : {
+        ac: 12, 
+        str: 10,
         size: sizeMedium - .5 //Low end of medium
     },
     .5 : {
+        ac: 12, 
+        str: 11,
         size: sizeMedium - .4
     },
     1 : {
+        ac: 13, 
+        str: 13,
         size: sizeMedium - .25
     },
     2 : {
+        ac: 13, 
+        str: 14,
         size: sizeMedium
     },
     3 : {
+        ac: 14, 
+        str: 14,
         size: sizeMedium + .25
     },
     4 : {
+        ac: 14, 
+        str: 15,
         size: sizeMedium + .4 //High end of medium
     },
     5 : {
+        ac: 14, 
+        str: 16,
         size: sizeLarge - .5 //Low end of large
     },
     6 : {
+        ac: 15, 
+        str: 16,
         size: sizeLarge - .4
     },
     7 : {
+        ac: 15, 
+        str: 17,
         size: sizeLarge - .3
     },
     8 : {
+        ac: 15, 
+        str: 17,
         size: sizeLarge - .25
     },
     9 : {
+        ac: 15, 
+        str: 17,
         size: sizeLarge - .15
     },
     10 : {
+        ac: 16, 
+        str: 18,
         size: sizeLarge
     },
     11 : {
+        ac: 16, 
+        str: 18,
         size: sizeLarge + .1
     },
     12 : {
+        ac: 16, 
+        str: 18,
         size: sizeLarge + .2
     },
     13 : {
+        ac: 17, 
+        str: 19,
         size: sizeLarge + .3
     },
     14 : {
+        ac: 17, 
+        str: 19,
         size: sizeHuge - .5
     },
     15 : {
+        ac: 18, 
+        str: 20,
         size: sizeHuge - .4
     },
     16 : {
+        ac: 18, 
+        str: 21,
         size: sizeHuge - .3
     },
     17 : {
+        ac: 19, 
+        str: 21,
         size: sizeHuge - .2
     },
     18 : {
+        ac: 19, 
+        str: 22,
         size: sizeHuge
     },
     19 : {
+        ac: 19, 
+        str: 23,
         size: sizeHuge + .1
     },
     20 : {
+        ac: 19, 
+        str: 24,
         size: sizeHuge + .2
     },
     21 : {
+        ac: 20, 
+        str: 25,
         size: sizeHuge + .3
     },
     22 : {
+        ac: 20, 
+        str: 26,
         size: sizeHuge + .4
     },
     23 : {
+        ac: 20, 
+        str: 27,
         size: sizeGargantuan - .2
     },
     24 : {
+        ac: 21, 
+        str: 27,
         size: sizeGargantuan
     },
     25 : {
+        ac: 22, 
+        str: 28,
         size: sizeGargantuan
     },
     26 : {
+        ac: 22, 
+        str: 28,
         size: sizeGargantuan
     },
     27 : {
+        ac: 23, 
+        str: 29,
         size: sizeGargantuan
     },
     28 : {
+        ac: 23, 
+        str: 29,
         size: sizeGargantuan
     },
     29 : {
+        ac: 24, 
+        str: 30,
         size: sizeGargantuan
     },
     30 : {
+        ac: 25, 
+        str: 30,
         size: sizeGargantuan
     }
 }
